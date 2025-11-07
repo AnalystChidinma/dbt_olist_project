4a6061968a8ee21383905df74ed3d70f9af24673
David Effiong <davideffiong@Davids-MacBook-Pro-M4.local>
2025-11-07 16:22:11 +0100

    update folders

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2025-11-07T15:20:02.375825+00:00
+-- finished_at: 2025-11-07T15:20:02.833068+00:00
+-- elapsed: 457ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c03bf8-0001-38db-0001-26120080c006
+-- desc: not provided
+select 1 as id;
